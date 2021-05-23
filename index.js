d172966b9efed7790dc304b06875a025e4ef71eb
ShevDenny <sdenny904@gmail.com>
2021-05-22 20:26:27 -0400

    commit tree 36bfb2e7d51a60dd9282eb16699ebaba47022773

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "John"
+
+upperCaseCustomerName = () => {customerName = customerName.toUpperCase()};
+
+setBestCustomer = () => {bestCustomer = "not bob"};
+
+overwriteBestCustomer = () => {bestCustomer = "maybe bob"};
+
+changeLeastFavoriteCustomer = () => {leastFavoriteCustomer = "not John"}
